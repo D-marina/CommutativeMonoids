7d75cdf661112faea59457ae20f905641cababde
D-marina <daniel.marinaragon@alum.uca.es>
2019-03-11 22:05:57 +0100

    Put some functions from auxiliars to auxiliars2

--- Class/auxiliars2.py
+++ Class/auxiliars2.py
@@ -1,3 +1,5 @@
+# Function for computing the Frobenius Number of a semigroup.
+
 def FrobeniusNumber(generators,eDimension=0):
     if eDimension == 0:
         eDimension = len(generators)
@@ -35,3 +37,51 @@ def FrobeniusNumber(generators,eDimension=0):
     aux = [S[i] for i in range(0,a)]
     fNumber = max(aux)-a
     return(fNumber)
+
+# Function to compute integer solutions of a Diophantine Equation
+
+def FSolve(lgen,x,onlyFirst=True):
+    posAModificar=0
+    sumando=True
+    dim = len(lgen)
+    ceros=np.array([0 for i in range(dim)],dtype=np.int)
+    xaux=x
+    tuplaActual=array([0 for i in range(dim)],dtype=np.int)  
+    if x==0:
+        return tuplaActual
+    soluciones=[]
+    while( not( np.all(tuplaActual==ceros) and not(sumando) ) ):
+        if sumando:
+            if xaux>=lgen[0]:
+                tuplaActual[posAModificar]=tuplaActual[posAModificar]+1
+                xaux=xaux-lgen[posAModificar]
+                if xaux==0:
+                    if onlyFirst:
+                        return tuplaActual
+                    else:
+                        soluciones.append(array(tuplaActual))
+                        sumando=False
+                        continue
+            if xaux!=0 and xaux<lgen[0]:
+                sumando=False
+                continue
+        else:
+            if ( posAModificar == dim-1 ) and ( tuplaActual[posAModificar] > 0 ):
+                xaux=tuplaActual[posAModificar]*lgen[posAModificar]+xaux
+                tuplaActual[posAModificar]=0
+                posAModificar=posAModificar-1
+                continue
+            if ( posAModificar < dim-1 ) and ( tuplaActual[posAModificar] > 0 ):
+                xaux=xaux+lgen[posAModificar]
+                tuplaActual[posAModificar]=tuplaActual[posAModificar]-1
+                posAModificar=posAModificar+1
+                sumando=True
+                continue
+            if ( posAModificar < dim-1 ) and ( tuplaActual[posAModificar] == 0 ):
+                posAModificar=np.max( [i for i in range(dim) if tuplaActual[i]!=0] )
+                xaux=lgen[posAModificar]+xaux
+                tuplaActual[posAModificar]=tuplaActual[posAModificar]-1
+                sumando=True
+                posAModificar=posAModificar+1
+                continue
+    return soluciones
