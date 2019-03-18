cbc59095dada989a5d51bfc0613195960a50eff8
D-marina <daniel.marinaragon@alum.uca.es>
2019-03-18 12:01:15 +0100

    Several changes

--- Class/auxiliars2.py
+++ Class/auxiliars2.py
@@ -3,6 +3,9 @@ from numpy import *
 import sympy
 from sympy import *
 
+import auxiliars
+from auxiliars import *
+
 # Function for computing the Frobenius Number of a semigroup.
 
 def FrobeniusNumber(generators,eDimension=0):
@@ -132,7 +135,81 @@ def Belong(generators,x,multiplicity=0,fNumber=0):
         return False
     
 
+# This function compute the Ns of a numerical semigroup.
 
+def ComputeNs(generators,dimension=0):
+    # Calculamos la dimension
+    a = generators
+    if dimension == 0:
+        dimension = len(a)
+    # Creamos los simbolos ei
+    e = symbols('e0:%d'%dimension)
+    # Creamos el simbolo s
+    s = symbols('s')
+    # Calculamos d
+    m = equationsToGeneratorsHomogeneusCase(Matrix([generators]))
+    m1 = sympyMatrix2numpyArray(m)
+    l = numpy.sum(m1,axis=1) # Esto es lo que fallaba porque l es de la forma [a b c] y deberia ser [a, b, c] con las comas, asi que lo transformo aqui.
+    lista = []
+    for i in range(len(l)):
+        lista.append(l[i])
+    d = gcdL(lista)
+    # Calculo un vector con los mcd
+    mcd = []
+    for i in range(dimension-2):
+        mcd.append(gcdL([a[i+1]-a[-1],-a[0]+a[-1],a[0]-a[i+1]]))
+    # Calculo h
+    h=d/(a[-1]-a[0])*(a[-1]*e[0]-a[0]*e[-1])
+    # Calculo P2
+    P2=s*(a[1]-a[-1])/(a[1]*(a[0]-a[-1]))*e[0]+s*(a[0]-a[1])/(a[1]*(a[0]-a[-1]))*e[-1]
+    # Calculo Pp-1
+    Ppm1=s*(a[-2]-a[-1])/(a[-2]*(a[0]-a[-1]))*e[0]+s*(a[0]-a[-2])/(a[-2]*(a[0]-a[-1]))*e[-1]
+    # Calculo un vector con los qi
+    Q = []
+    for i in range(dimension-2):
+        Q.append(1/mcd[i]*((a[i+1]-a[-1])*e[0]+(a[-1]-a[0])*e[i+1]+(a[0]-a[i+1])*e[-1]))
+    # Calculo la suma de los Qi
+    sumaQi = 0
+    for i in range(dimension-2):
+        sumaQi += Q[i]
+    # Calculamos la última coordenada P2+h+∑qi
+    primeraEcuacion = P2+h+sumaQi
+    for i in range(dimension-1):
+        primeraEcuacion = primeraEcuacion.subs(e[i],0)
+    primeraEcuacion = primeraEcuacion.subs(e[dimension-1],1)
+    sol1 = sympy.solve(primeraEcuacion,s)
+    # Calculamos la primera coordenada de P(p−1)−h+∑qi
+    ultimaEcuacion = Ppm1-h+sumaQi
+    for i in range(1,dimension):
+        ultimaEcuacion = ultimaEcuacion.subs(e[i],0)
+    ultimaEcuacion = ultimaEcuacion.subs(e[0],1)
+    sol2 = sympy.solve(ultimaEcuacion,s)
+    NS = int(ceil(int(max(sol1,sol2)[0])))
+    return NS    
+    
+# This function compute Lambda_1 for computing the Delta_nu
+
+def CalcularLambda1(lgen,dimension = 0,Ns=0):
+    if Ns == 0:
+        ComputeNs(lgen,dimension)
+    c1 = (lgen[-1]-lgen[-2])/lgen[-2]*Ns
+    c4 = (lgen[0]/lgen[-2]-lgen[0]/lgen[-1]-lgen[0]/lgen[1]+1)*Ns
+    return max(c1,c4)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 def f1(e, n):
     '''
     Function to compute the way to distribute n iqual objects in e different positions
