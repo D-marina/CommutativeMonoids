3cebef385c5250f0d73b94d5d0961b998720f7ea
D-marina <daniel.marinaragon@alum.uca.es>
2019-05-06 13:26:16 +0200

    Add monoidsCpp.h

---
+++ CClass/monoidsCpp.h
@@ -0,0 +1,13 @@
+#ifndef _BAR_H 
+#define _BAR_H 
+
+#include <vector> 
+#include <algorithm>
+#include <iostream>
+
+long FrobeniusNumber(std::vector<long> lgen);
+
+std::vector<std::vector<long> > FSolve(std::vector<long> lgen, long x);
+
+#endif
+
