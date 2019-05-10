ae091952709a1ccf806caf2372288787f2133918
D-marina <daniel.marinaragon@alum.uca.es>
2019-05-10 13:44:51 +0200

    working on Ns

--- CClass/monoidsCpp.h
+++ CClass/monoidsCpp.h
@@ -4,6 +4,7 @@
 #include <vector> 
 #include <algorithm>
 #include <iostream>
+#include <cmath>
 
 long FrobeniusNumber(std::vector<long> lgen);
 
@@ -12,10 +13,22 @@ std::vector<std::vector<long> > FSolve(std::vector<long> lgen, long x);
 std::vector<long> smgS(std::vector<long> gen);
 
 void Pintar(std::vector<long> v);
+void Pintar(std::vector<double> v);
 
 bool Belong(std::vector<long> generators,long x);
 
 bool Belong(std::vector<long> generators,long x, long fNumber);
+
+long ComputeD(std::vector<long> generators);
+
+long ComputeNs(std::vector<long> a);
+
+long gcd(long a, long b);
+
+long gcdL(std::vector<long> v);
+
+long maximum(std::vector<long> v);
+double maximum(std::vector<double> v);
 
 #endif
 
