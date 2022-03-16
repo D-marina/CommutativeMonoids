9b9f2e4fa9b007ee8a42449a3fbfea2288eb20a5
D-marina <daniel.marinaragon@alum.uca.es>
2022-03-16 11:13:22 +0100

    fixed

--- CClass/setup.py
+++ CClass/setup.py
@@ -1,6 +1,6 @@
 import distutils 
 from distutils.core import setup, Extension 
 
-monoids = Extension("_monoids",sources = ['monoidsCpp.i','monoidsCpp.cxx',],swig_opts = ['-Wall','-c++'],) 
+monoidsCpp = Extension("_monoids",sources = ['monoidsCpp.i','monoidsCpp.cxx',],swig_opts = ['-Wall','-c++'],) 
 
-setup( name = 'monoids', ext_modules = [monoids,],  ) 
+setup( name = 'monoids', ext_modules = [monoidsCpp,],  ) 
