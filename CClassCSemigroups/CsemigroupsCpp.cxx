d3321bb0e326cf7a7c19b7c6986c4d48b9df0416
D-marina <daniel.marinaragon@alum.uca.es>
2022-03-16 23:57:54 +0100

    Add a function in C

--- CClassCSemigroups/CsemigroupsCpp.cxx
+++ CClassCSemigroups/CsemigroupsCpp.cxx
@@ -6,3 +6,67 @@ int foo(int a)
 {
     return a+1;
 }
+
+void Pintar(vector<long> v)
+{
+	cout<<"*";
+	for(unsigned ii=0;ii<v.size();ii++)
+		cout<<","<<v[ii];
+	cout<<"*";
+	cout<<endl;
+}
+
+vector<long> operator-(const vector<long>& v1, const vector<long>& v2)
+{
+    unsigned n;
+    n = v1.size();
+    vector<long> aux(n);
+    for(unsigned ii=0;ii<n;ii++)
+    {
+        aux[ii] = v1[ii]-v2[ii];
+    }
+    return aux;
+}
+
+bool belongByGens(vector<long> x, vector<vector<long>> gen)
+{
+    /*
+    This function checks if x can be generated using the
+    elementos of gen.
+    */
+    unsigned n,m;
+    bool allZero;
+    n = gen.size();
+    m = x.size();
+    if(n == 0)
+        return false;
+    allZero = true;
+    for(unsigned ii=0;ii<m;ii++)
+    {
+        if(x[ii]!=0)
+            allZero = false;
+        if(x[ii]<0)
+            return false;
+    }
+    if(allZero)
+        return true;
+    if(belongByGens(x-gen[0], gen))
+    {
+        return true;
+    }
+    else
+    {
+        if(gen.size() == 1)
+        {
+            return false;
+        }
+        else
+        {
+            vector<vector<long>> aux(n-1);
+            for(unsigned ii=0;ii<n-1;ii++)
+                aux[ii] = gen[ii+1];
+            return belongByGens(x,aux);
+        }
+    }
+    return false;
+}
--- CClassCSemigroups/CsemigroupsCpp.h
+++ CClassCSemigroups/CsemigroupsCpp.h
@@ -6,10 +6,10 @@
 #include <iostream>
 #include <cmath>
 
-
-// codigo
-
 int foo (int a);
+void Pintar(std::vector<long> v);
+std::vector<long> operator-(const std::vector<long>& v1, const std::vector<long>& v2);
+bool belongByGens(std::vector<long> x, std::vector<std::vector<long>> gen);
 
 #endif
 
